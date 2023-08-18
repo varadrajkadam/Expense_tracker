a553c0a7ecfd327a7cbe051e4e205ff0fd4e0453
varadraj <varadrajkadam11vk@gmail.com>
2023-08-18 11:44:00 +0530

    Trying for Deployment

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ mongoose
   })
   .catch((err) => console.error(err));
 
-if (process.env.NODE_ENV == "production") {
+// if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", function (req, res) {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+// }
